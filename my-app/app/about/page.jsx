6ef2c5f09e4256bbabd7859ae347a1c116046958
javed ahmad <ja8688126@gmail.com>
2026-01-04 00:14:05 +0530

    practice routing and navigation and path by using usePathname

--- my-app/app/about/page.jsx
+++ my-app/app/about/page.jsx
@@ -1,9 +1,18 @@
+"use client";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 export default function page() {
+  const router = useRouter();
   return (
-    <div className="flex justify-center items-center h-[90vh]">
+    <div className="flex flex-col gap-4 justify-center items-center h-[90vh]">
       <h2 className="text-2xl underline">This is About Page</h2>
+      <button
+        onClick={() => router.push("/")}
+        className="hover:underline cursor-pointer"
+      >
+        Go To Home{" "}
+      </button>
     </div>
   );
 }
--- my-app/app/components/Navbar.jsx
+++ my-app/app/components/Navbar.jsx
@@ -1,26 +1,53 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const Navbar = () => {
+  const path = usePathname();
+  console.log("path is", path);
+  // const cleanPath = path.replace("/", "");
+  // console.log("cleanPath is", cleanPath);
+
   return (
     <div className="flex justify-between items-center px-10 py-5 bg-blue-900">
-      <Link href={"/"} className="hover:text-orange-500 text-xl">
-        ShopEase
+      <Link href={"/"} className="hover:text-orange-500 text-2xl font-bold">
+        <span className="text-green-500">Shop</span>Ease
       </Link>
       <div className="flex gap-4">
-        <Link href={"/"} className="hover:text-orange-500">
+        <Link
+          href={"/"}
+          className={`hover:text-orange-500 ${
+            path === "/" ? "text-orange-500" : ""
+          }`}
+        >
           {" "}
           Home
         </Link>
-        <Link href={"/about"} className="hover:text-orange-500">
+        <Link
+          href={"/about"}
+          className={`hover:text-orange-500 ${
+            path === "/about" ? "text-orange-500" : ""
+          }`}
+        >
           {" "}
           About
         </Link>
-        <Link href={"/contact"} className="hover:text-orange-500">
+        <Link
+          href={"/contact"}
+          className={`hover:text-orange-500 ${
+            path === "/contact" ? "text-orange-500" : ""
+          }`}
+        >
           {" "}
           Contact
         </Link>
-        <Link href={"/product"} className="hover:text-orange-500">
+        <Link
+          href={"/product"}
+          className={`hover:text-orange-500 ${
+            path === "/product" ? "text-orange-500" : ""
+          }`}
+        >
           {" "}
           Product
         </Link>
